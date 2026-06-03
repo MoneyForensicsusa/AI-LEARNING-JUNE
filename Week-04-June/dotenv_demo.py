f475e2dbf987d14f76328c212445bcc91df0f5d6
Wonderful Eyeh <wonderfuleyeh25@gmail.com>
2026-06-03 18:09:00 -0500

    added MY_TRACK to .env

--- Week-04-June/dotenv_demo.py
+++ Week-04-June/dotenv_demo.py
@@ -5,6 +5,8 @@ load_dotenv()
 
 token = os.getenv("GITHUB_TOKEN")
 name = os.getenv("MY_NAME")
+track = os.getenv("MY_TRACK")
 
 print(f"Token: {token}")
 print(f"Name: {name}")
+print(f"Track: {track}")
